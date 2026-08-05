dac35b90d507dda07411c77193a6095653503071
Tiago Rivero <tiagorivero100@gmail.com>
2026-08-05 00:38:13 -0300

    Add message persistence with Turso and random usernames

--- 2 - Real-Time Chat/server/index.js
+++ 2 - Real-Time Chat/server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import logger from "morgan"
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -12,17 +16,55 @@ const io = new Server(server, {
   connectionStateRecovery:{}
 })
 
-io.on('connection',(socket) =>{
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+  `)
+
+io.on('connection', async (socket) =>{
   console.log('a user has connected!')
 
   socket.on('disconnect', () =>{
     console.log('an user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg)
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    try{
+      result = await db.execute({
+        sql: `INSERT INTO messages (content, user) VALUES (:msg, :username)`,
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+
+  if (!socket.recovered){
+    try{
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  } 
 })
 
 app.use(logger('dev'))
